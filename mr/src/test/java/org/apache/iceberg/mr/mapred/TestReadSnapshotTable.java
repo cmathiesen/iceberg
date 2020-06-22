85b68233218749472597cb92284f97a7fb85b1ad
awoodhead <awoodhead@expediagroup.com>
2020-06-22 15:30:11 +0100

    tidy up and test fixes/ignore

---
+++ mr/src/test/java/org/apache/iceberg/mr/mapred/TestReadSnapshotTable.java
@@ -0,0 +1,200 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.iceberg.mr.mapred;
+
+import com.klarna.hiverunner.HiveShell;
+import com.klarna.hiverunner.StandaloneHiveRunner;
+import com.klarna.hiverunner.annotations.HiveSQL;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.iceberg.DataFile;
+import org.apache.iceberg.FileFormat;
+import org.apache.iceberg.PartitionSpec;
+import org.apache.iceberg.Schema;
+import org.apache.iceberg.Snapshot;
+import org.apache.iceberg.Table;
+import org.apache.iceberg.catalog.TableIdentifier;
+import org.apache.iceberg.data.Record;
+import org.apache.iceberg.hadoop.HadoopCatalog;
+import org.apache.iceberg.relocated.com.google.common.collect.Lists;
+import org.apache.iceberg.types.Types;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+
+import static org.apache.iceberg.types.Types.NestedField.optional;
+import static org.apache.iceberg.types.Types.NestedField.required;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(StandaloneHiveRunner.class)
+public class TestReadSnapshotTable {
+
+  @HiveSQL(files = {}, autoStart = true)
+  private HiveShell shell;
+
+  @Rule
+  public TemporaryFolder temp = new TemporaryFolder();
+
+  private File tableLocation;
+  private Configuration conf = new Configuration();
+  private HadoopCatalog catalog;
+  private Schema schema = new Schema(required(1, "id", Types.LongType.get()),
+      optional(2, "data", Types.StringType.get()));
+  private long snapshotId;
+
+  @Before
+  public void before() throws IOException {
+    tableLocation = temp.newFolder();
+    catalog = new HadoopCatalog(conf, tableLocation.getAbsolutePath());
+    PartitionSpec spec = PartitionSpec.unpartitioned();
+
+    TableIdentifier id = TableIdentifier.parse("source_db.table_a");
+    Table table = catalog.createTable(id, schema, spec);
+
+    List<Record> data = new ArrayList<>();
+    data.add(TestHelpers.createSimpleRecord(1L, "Michael"));
+    data.add(TestHelpers.createSimpleRecord(2L, "Andy"));
+    data.add(TestHelpers.createSimpleRecord(3L, "Berta"));
+
+    DataFile fileA = TestHelpers.writeFile(temp.newFile(), table, null, FileFormat.PARQUET, data);
+    DataFile fileB = TestHelpers.writeFile(temp.newFile(), table, null, FileFormat.PARQUET, data);
+    DataFile fileC = TestHelpers.writeFile(temp.newFile(), table, null, FileFormat.PARQUET, data);
+    table.newAppend().appendFile(fileA).commit();
+    table.newAppend().appendFile(fileB).commit();
+    table.newAppend().appendFile(fileC).commit();
+
+    List<Snapshot> snapshots = Lists.newArrayList(table.snapshots().iterator());
+    snapshotId = snapshots.get(0).snapshotId();
+  }
+
+  @Ignore("TODO: re-enable this test when snapshot functionality added")
+  @Test
+  public void testReadSnapshotTable() {
+    shell.execute("CREATE DATABASE source_db");
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_a ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_a'")
+        .toString());
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_a__snapshots ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_a'")
+        .toString());
+
+    List<Object[]> result = shell.executeStatement("SELECT * FROM source_db.table_a__snapshots");
+
+    assertEquals(3, result.size());
+  }
+
+  @Ignore("TODO: re-enable this test when snapshot functionality added")
+  @Test
+  public void testCreateRegularTableEndingWithSnapshots() throws IOException {
+    TableIdentifier id = TableIdentifier.parse("source_db.table_a__snapshots");
+    Table table = catalog.createTable(id, schema, PartitionSpec.unpartitioned());
+
+    List<Record> data = new ArrayList<>();
+    data.add(TestHelpers.createSimpleRecord(1L, "Michael"));
+    DataFile fileA = TestHelpers.writeFile(temp.newFile(), table, null, FileFormat.PARQUET, data);
+    table.newAppend().appendFile(fileA).commit();
+
+    shell.execute("CREATE DATABASE source_db");
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_a__snapshots ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_a__snapshots")
+        .append("' TBLPROPERTIES ('iceberg.snapshots.table'='false')")
+        .toString());
+
+    List<Object[]> result = shell.executeStatement("SELECT * FROM source_db.table_a__snapshots");
+
+    assertEquals(1, result.size());
+  }
+
+  @Ignore("TODO: re-enable this test when snapshot functionality added")
+  @Test
+  public void testTimeTravelRead() {
+    shell.execute("CREATE DATABASE source_db");
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_a ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_a'")
+        .toString());
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_a__snapshots ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_a'")
+        .toString());
+
+    List<Object[]> resultLatestTable = shell.executeStatement("SELECT * FROM source_db.table_a");
+    assertEquals(9, resultLatestTable.size());
+
+    List<Object[]> resultFirstSnapshot = shell.executeStatement(
+        "SELECT * FROM source_db.table_a WHERE SNAPSHOT__ID = " + snapshotId);
+    assertEquals(3, resultFirstSnapshot.size());
+
+    List<Object[]> resultLatestSnapshotAgain = shell.executeStatement("SELECT * FROM source_db.table_a");
+    assertEquals(9, resultLatestSnapshotAgain.size());
+  }
+
+  @Test
+  public void testCreateTableWithSnapshotIDColumnInSchema() throws IOException {
+    PartitionSpec spec = PartitionSpec.unpartitioned();
+    schema = new Schema(required(1, "snapshot__id", Types.LongType.get()),
+        optional(2, "data", Types.StringType.get()));
+    TableIdentifier id = TableIdentifier.parse("source_db.table_b");
+    Table table = catalog.createTable(id, schema, spec);
+
+    List<Record> data = new ArrayList<>();
+    data.add(TestHelpers.createSimpleRecord(1L, "Michael"));
+    DataFile fileA = TestHelpers.writeFile(temp.newFile(), table, null, FileFormat.PARQUET, data);
+    table.newAppend().appendFile(fileA).commit();
+
+    shell.execute("CREATE DATABASE source_db");
+
+    shell.execute(new StringBuilder()
+        .append("CREATE TABLE source_db.table_b ")
+        .append("STORED BY 'org.apache.iceberg.mr.mapred.IcebergStorageHandler' ")
+        .append("LOCATION '")
+        .append(tableLocation.getAbsolutePath() + "/source_db/table_b")
+        .append("' TBLPROPERTIES (")
+        .append("'iceberg.hive.snapshot.virtual.column.name' = 'metadata_snapshot_id')")
+        .toString());
+
+    List<Object[]> resultLatestTable = shell.executeStatement("SELECT * FROM source_db.table_b");
+    assertEquals(1, resultLatestTable.size());
+  }
+}
